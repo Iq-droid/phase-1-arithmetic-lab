730c5193bdaffad6c767bc235ed1010c0dee8cfe
Iqra <iqra.ali@student.moringaschool.com>
2023-03-23 01:37:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
 
-// Write your code here
+var random = Math.floor(Math.random() * 10) + 1;
+
+let remainder = 36 % 16;
+
+const arr = [0, 5, 15, 20];
+const max = Math.max(...arr);
